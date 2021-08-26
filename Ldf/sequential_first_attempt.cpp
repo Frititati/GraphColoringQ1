cf4610963d3f3a851ff647ca5801b0df1674472b
Filippo Maria Cardano <filippo1mc@gmail.com>
2021-08-27 00:13:37 +0200

    LDF sequential cleanup

--- Ldf/sequential_first_attempt.cpp
+++ Ldf/sequential_first_attempt.cpp
@@ -13,24 +13,25 @@
 using namespace std;
 
 // the datastructure where we keep the node index and all of it's connections
-map<int, vector<int> > node_edge_connections;
+map < int, vector < int > > node_edge_connections;
 // the datastructure where we keep the node random based on index
-vector<int> node_random;
+vector < int > node_random;
 // the datastructure where we keep the node color based on index
-vector<int> node_color;
+vector < int > node_color;
 // the datastructure where we keep the degree of each node
-vector<int> node_degree;
+vector < int > node_degree;
 
 bool directed = 0;
 
-vector<string> split(string s, string delimiter)
-{
+// path to the graph
+string graph_path;
+
+vector < string > split(string s, string delimiter) {
 	size_t pos_start = 0, pos_end, delim_len = delimiter.length();
 	string token;
-	vector<string> res;
+	vector < string > res;
 
-	while ((pos_end = s.find(delimiter, pos_start)) != string::npos)
-	{
+	while ((pos_end = s.find(delimiter, pos_start)) != string::npos) {
 		token = s.substr(pos_start, pos_end - pos_start);
 		pos_start = pos_end + delim_len;
 		res.push_back(token);
@@ -40,153 +41,122 @@ vector<string> split(string s, string delimiter)
 	return res;
 }
 
-vector<int> split_to_int(string s, string delimiter)
-{
+vector < int > split_to_int(string s, string delimiter) {
 	size_t pos_start = 0, pos_end, delim_len = delimiter.length();
 	string token;
-	vector<int> res;
+	vector < int > res;
 
-	while ((pos_end = s.find(delimiter, pos_start)) != string::npos)
-	{
+	while ((pos_end = s.find(delimiter, pos_start)) != string::npos) {
 		token = s.substr(pos_start, pos_end - pos_start);
 		pos_start = pos_end + delim_len;
-		try
-		{
+		try {
 			res.push_back(stoi(token));
-		}
-		catch (exception e)
-		{
+		} catch (exception e) {
 			// we don't have any node connections
 			// cout << "There are no connections" << endl;
 		}
 	}
 
-	try
-	{
+	try {
 		res.push_back(stoi(s.substr(pos_start)));
-	}
-	catch (exception e)
-	{
+	} catch (exception e) {
 		// we don't have any node connections
 		// cout << "There are no connections" << endl;
 	}
 	return res;
 }
 
-vector<int> split_to_int_mod(string line, string delimiter)
-{
+vector < int > split_to_int_mod(string line, string delimiter) {
 	string s;
 	int start_point = line.find(':') + 2;
 	int end_point = line.find("#");
 	int line_size = end_point - 1 - start_point;
-	if (end_point != start_point)
+	if (end_point != start_point) {
 		s = line.substr(start_point, line_size);
-	else
+	} else {
 		s = "";
-	// cout << "modified line: -" << s << "-" << endl;
+	}
 
 	size_t pos_start = 0, pos_end, delim_len = delimiter.length();
 	string token;
-	vector<int> res;
-	while ((pos_end = s.find(delimiter, pos_start)) != string::npos)
-	{
+	vector < int > res;
+	while ((pos_end = s.find(delimiter, pos_start)) != string::npos) {
 		token = s.substr(pos_start, pos_end - pos_start);
 		pos_start = pos_end + delim_len;
-		try
-		{
+		try {
 			res.push_back(stoi(token) + 1);
-		}
-		catch (exception e)
-		{
-			cout << "we had an OPSY" << endl;
+		} catch (exception e) {
+			// we don't have any node connections
+			// cout << "There are no connections" << endl;
 		}
 	}
 
-	try
-	{
+	try {
 		res.push_back(stoi(s.substr(pos_start)) + 1);
-	}
-	catch (exception e)
-	{
-		cout << "we had an OPSY" << endl;
+	} catch (exception e) {
+		// we don't have any node connections
+		// cout << "There are no connections" << endl;
 	}
 	return res;
 }
 
-int ldf_sequential()
-{
-	// first we look for the highest local MIS
-
-	// iterate on the hash map
-
+int ldf_sequential() {
+	// color is defined as integer
 	int color = 1;
-
-	set<int> colors_used_global = {1};
+	// we have a set of the colors used (this is useful to compare later in the iterative step)
+	set < int > colors_used_global = { 1 };
 
 	bool first_iter = true;
 
-	while (node_edge_connections.size() != 0)
-	{
-		// map of all the nodes to be evaluated this iteration <index, color>
-		map<int, int> to_be_evaluated;
+	while (node_edge_connections.size() != 0) {
 
-		// cout << "size node " << to_string(node_edge_connections.size()) << " color " << color << endl;
+		// map of all the nodes to be evaluated this iteration <index, color>
+		map < int, int > to_be_evaluated;
 
 		// iterate on the map
-		for (map<int, vector<int> >::const_iterator it = node_edge_connections.begin();
-			 it != node_edge_connections.end(); ++it)
-		{
+		for (map < int, vector < int > > ::const_iterator it = node_edge_connections.begin(); it != node_edge_connections.end(); ++it) {
+
 			// vector of all the connected nodes
-			vector<int> connections = it->second;
+			vector < int > connections = it -> second;
 
 			bool is_highest = true;
 
-			set<int> colors_used_local;
+			set < int > colors_used_local;
 
-			int node_random_this = node_random[it->first - 1];
-			int node_degree_this = node_degree[it->first - 1];
-			int node_key_this = it->first;
+			int node_random_this = node_random[it -> first - 1];
+			int node_degree_this = node_degree[it -> first - 1];
+			int node_key_this = it -> first;
 
 			// look into all the connected nodes
-			for (auto i : connections)
-			{
+			for (auto i: connections) {
 				// check if connections is not colored
-				if (node_color[i - 1] == 0)
-				{
+				if (node_color[i - 1] == 0) {
 					if (node_degree_this < node_degree[i - 1] ||
 						(node_degree_this == node_degree[i - 1] && node_random_this < node_random[i - 1]) ||
-						(node_degree_this == node_degree[i - 1] && node_random_this == node_random[i - 1] && node_key_this < i))
-					{
+						(node_degree_this == node_degree[i - 1] && node_random_this == node_random[i - 1] && node_key_this < i)) {
 						// not local highest
 						is_highest = false;
 						break;
 					}
-				}
-				else
-				{
+				} else {
 					colors_used_local.insert(node_color[i - 1]);
 				}
 			}
 
 			// only execute if the node is the highest in its local section
-			if (is_highest)
-			{
-				// cout << "node " << node_key_this << " highest" << endl;
-				std::set<int> colors_used_interation;
-				std::set_difference(colors_used_global.begin(), colors_used_global.end(), colors_used_local.begin(), colors_used_local.end(), std::inserter(colors_used_interation, colors_used_interation.end()));
-				auto choosen = colors_used_interation.begin();
-				to_be_evaluated.insert(std::pair<int, int>(node_key_this, *choosen));
+			if (is_highest) {
+				std::set < int > colors_used_iteration;
+				std::set_difference(colors_used_global.begin(), colors_used_global.end(), colors_used_local.begin(), colors_used_local.end(), std::inserter(colors_used_iteration, colors_used_iteration.end()));
+				auto choosen = colors_used_iteration.begin();
+				to_be_evaluated.insert(std::pair < int, int > (node_key_this, * choosen));
 			}
 		}
 
-		for (map<int, int>::const_iterator node_interator = to_be_evaluated.begin();
-			 node_interator != to_be_evaluated.end(); ++node_interator)
-		{
-			node_color[node_interator->first - 1] = node_interator->second;
-			node_edge_connections.erase(node_interator->first);
+		for (map < int, int > ::const_iterator node_interator = to_be_evaluated.begin(); node_interator != to_be_evaluated.end(); ++node_interator) {
+			node_color[node_interator -> first - 1] = node_interator -> second;
+			node_edge_connections.erase(node_interator -> first);
 
-			if (node_interator->second == color)
-			{
+			if (node_interator -> second == color) {
 				color++;
 				colors_used_global.insert(color);
 			}
@@ -196,24 +166,20 @@ int ldf_sequential()
 	return 0;
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char ** argv) {
 
-	if (argc < 2)
-	{
+	if (argc < 2) {
 		// not enought parameters
 		cout << "Not enough parameters passed, please refer to this guide:" << endl;
 		cout << "<execution file> <graph file>" << endl;
 		return 1;
 	}
 
-	// TODO : we have to check if this is exeception prone
-
 	fstream graph_file;
-	graph_file.open(argv[1], ios::in);
+	graph_path = argv[1];
+	graph_file.open(graph_path, ios:: in );
 
-	if (!graph_file.is_open())
-	{
+	if (!graph_file.is_open()) {
 		// file does not exists
 		cout << "File cannot be found, please refer to this guide:" << endl;
 		cout << "<execution file> <graph file>" << endl;
@@ -226,24 +192,24 @@ int main(int argc, char **argv)
 	int number_edges;
 
 	// we used srand to set seed for randomization of node numbers
-	// srand(time(NULL));
+	srand(time(NULL));
 
-	if (graph_file.is_open())
-	{
+	auto start_read = chrono::steady_clock::now();
+
+	if (graph_file.is_open()) {
 
 		// this is first line
 		getline(graph_file, line);
-		vector<int> parse_first_line = split_to_int(line, " ");
+		vector < int > parse_first_line = split_to_int(line, " ");
 
 		number_nodes = parse_first_line[0];
 		directed = (parse_first_line.size() == 1);
 		if (!directed)
 			number_edges = parse_first_line[1];
 
-		while (getline(graph_file, line))
-		{
+		while (getline(graph_file, line)) {
 			// parsing node line
-			vector<int> temp;
+			vector < int > temp;
 			if (directed)
 				temp = split_to_int_mod(line, " ");
 			else
@@ -251,108 +217,96 @@ int main(int argc, char **argv)
 			// cout << "node number :" << to_string(new_node_index) << endl;
 			// for (auto i : temp) cout << "conn " << to_string(i) << endl;
 
-			node_edge_connections.insert(std::pair<int, vector<int> >(new_node_index, temp));
+			node_edge_connections.insert(std::pair < int, vector < int > > (new_node_index, temp));
 
 			// add random number to each node
 			node_random.push_back(rand() % 100 + 1);
 			// add color 0
 			node_color.push_back(0);
 			// cout << "Degree of node " << new_node_index << " is " << temp.size() << endl;
-			if (!directed)
+			if (!directed) {
 				node_degree.push_back(temp.size());
+			}
 			new_node_index++;
 		}
 	}
 
-	while (node_edge_connections.size() < number_nodes)
-	{
-		node_edge_connections.insert(std::pair<int, vector<int> >(new_node_index, {}));
+	while (node_edge_connections.size() < number_nodes) {
+		node_edge_connections.insert(std::pair < int, vector < int > > (new_node_index, {}));
 		node_random.push_back(rand() % 100 + 1);
 		node_color.push_back(0);
 		new_node_index++;
 	}
+
 	// handle directed graph
-	if (directed)
-	{
-		map<int, vector<int> > temp_node_edge = node_edge_connections;
+	if (directed) {
+		map < int, vector < int > > temp_node_edge = node_edge_connections;
 
 		// translate the graph
 
 		//for each node of the map
-		for (map<int, vector<int> >::const_iterator it = node_edge_connections.begin();
-			 it != node_edge_connections.end(); ++it)
-		{
+		for (map < int, vector < int > > ::const_iterator it = node_edge_connections.begin(); it != node_edge_connections.end(); ++it) {
 			//for each element of the selected array, insert the node it->first into the nodes referenced in the array
-			for (auto i : it->second)
-				temp_node_edge[i].push_back(it->first);
+			for (auto i: it -> second) {
+				temp_node_edge[i].push_back(it -> first);
+			}
 		}
 		node_edge_connections = temp_node_edge;
 
 		// compute number_edges and degrees
 
-		for (map<int, vector<int> >::const_iterator it = node_edge_connections.begin();
-			 it != node_edge_connections.end(); ++it)
-		{
+		for (map < int, vector < int > > ::const_iterator it = node_edge_connections.begin(); it != node_edge_connections.end(); ++it) {
 			// cout << "number_edges: " << number_edge
 			//edges
-			number_edges += it->second.size();
+			number_edges += it -> second.size();
 			//degree
-			node_degree.push_back(it->second.size());
+			node_degree.push_back(it -> second.size());
 		}
 	}
 
-	// print results
-
-	// cout << "Number of nodes: " << number_nodes << "\tNumber of edges: " << number_edges << endl;
-	// for (map<int, vector<int> >::const_iterator it = node_edge_connections.begin();
-	// 	 it != node_edge_connections.end(); ++it)
-	// {
-	// 	cout << "At node: " << it->first << "\t We have connections: ";
-	// 	for (auto i : it->second)
-	// 		cout << i << " ";
-
-	// 	cout << "\tDegree: " << node_degree[it->first - 1] << endl;
-	// }
-
 	graph_file.close();
 
-	// start a timer
-	auto start = chrono::steady_clock::now();
-
-	// unsync the I/O of C and C++.
+	auto start_algo = chrono::steady_clock::now();
 	ios_base::sync_with_stdio(false);
 
 	ldf_sequential();
 
-	// end timer
-	auto end = chrono::steady_clock::now();
+	auto start_write = chrono::steady_clock::now();
 
-	cout << "Elapsed time in nanoseconds: "
-		 << chrono::duration_cast<chrono::nanoseconds>(end - start).count()
-		 << " ns" << endl;
+	string final = to_string(number_nodes) + " " + to_string(number_edges) + "\n";
 
-	cout << "Elapsed time in microseconds: "
-		 << chrono::duration_cast<chrono::microseconds>(end - start).count()
-		 << " µs" << endl;
+	int max_color = 0;
 
-	fstream write_file;
-	write_file.open(argv[2], ios::out);
-
-	if (write_file.is_open())
-	{
-		int max = 0;
-		// if file exists write to file results
-		write_file << to_string(number_nodes) << " " << to_string(number_edges) << endl;
-		for (auto i : node_color)
-		{
-			write_file << to_string(i) << endl;
-			if (i > max)
-				max = i;
+	for (auto i: node_color) {
+		final += to_string(i) + "\n";
+		if (i > max_color) {
+			max_color = i;
 		}
-		cout << "Used colors: " << max << endl;
 	}
 
-	write_file.close();
+	auto output_file = std::fstream(argv[2], std::ios::out | std::ios::binary);
+	output_file.write(final.c_str(), (final.size() * sizeof(char)));
+	output_file.close();
+
+	auto end_write = chrono::steady_clock::now();
+
+	cout << "Elapsed reading time in microseconds: " << chrono::duration_cast < chrono::microseconds > (start_algo - start_read).count() << " µs" << endl;
+	cout << "Elapsed algo time in microseconds: " << chrono::duration_cast < chrono::microseconds > (start_write - start_algo).count() << " µs" << endl;
+	cout << "Elapsed writing time in microseconds: " << chrono::duration_cast < chrono::microseconds > (end_write - start_write).count() << " µs" << endl;
+	cout << "Elapsed time in microseconds: " << chrono::duration_cast < chrono::microseconds > (end_write - start_read).count() << " µs" << endl;
+
+	std::ofstream results_file;
+	results_file.open("ldf_seq_1.csv", std::ios_base::app);
+	results_file << graph_path.substr(graph_path.find_last_of("/\\") + 1) << "," <<
+		number_nodes << "," <<
+		number_edges << "," <<
+		"sequential" << "," <<
+		max_color << "," <<
+		chrono::duration_cast < chrono::microseconds > (end_write - start_read).count() << "," <<
+		chrono::duration_cast < chrono::microseconds > (start_algo - start_read).count() << "," <<
+		chrono::duration_cast < chrono::microseconds > (start_write - start_algo).count() << "," <<
+		chrono::duration_cast < chrono::microseconds > (end_write - start_write).count() << ",\n";
+	results_file.close();
 
 	return 0;
 }
