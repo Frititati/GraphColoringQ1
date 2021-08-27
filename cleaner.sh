808171cf8020ad9ac5575a0d047470378093e9b0
Filippo Maria Cardano <filippo1mc@gmail.com>
2021-08-27 15:15:56 +0200

    added cleaner.sh

---
+++ cleaner.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+rm Jones/test_multi_2
+rm Jones/test_multi_3
+rm Jones/test_multi_5
+rm Ldf/test_multi_1
+
+rm Jones/test_seq_1
+rm Jones/test_seq_2
+rm Jones/test_seq_2_spinoff
+rm Ldf/test_seq_1
